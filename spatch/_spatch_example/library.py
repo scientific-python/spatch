ace562369e0f92ac4378ab1d6719088eb8d6b7e5
Sebastian Berg <sebastianb@nvidia.com>
2025-05-14 11:51:43 -0700

    make it a backend context manager in the "library"

--- spatch/_spatch_example/library.py
+++ spatch/_spatch_example/library.py
@@ -1,13 +1,15 @@
 
 from spatch.backend_system import BackendSystem
 
-backend_system = BackendSystem(
+_backend_system = BackendSystem(
     "_spatch_example_backends",
     default_primary_types=["builtins:int"]
 )
 
 
-@backend_system.dispatchable(["x", "y"])
+backend = _backend_system.prioritize
+
+@_backend_system.dispatchable(["x", "y"])
 def divide(x, y):
     """Divide integers, other types may be supported via backends"""
     if not isinstance(x, int) or not isinstance(y, int):
