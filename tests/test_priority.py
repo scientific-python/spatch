e6d6099f5c6a8519b339827b55cbe1cca97dcc74
Sebastian Berg <sebastianb@nvidia.com>
2025-07-06 11:08:07 +0200

    ENH,MAINT: Rename "relevant" to "dispatch" args and expose in context (#13)
    This is useful for generic wrappers.  I still wan to hold back to do
    all the work for them, but the information is rather readily available,
    so I think we can pass it on.
    
    NumPy uses "relevant args" naming for the arguments we dispatch on,
    I decided to rename it to "dispatch" instead.
    (Also changed to pass a tuple of types, because I think that is more
    future proof.  We expect few types, a set isn't useful e.g. if done in C.)

--- tests/test_priority.py
+++ tests/test_priority.py
@@ -1,44 +1,7 @@
 import pytest
 
 from spatch.backend_system import BackendSystem
-from spatch.utils import get_identifier
-
-class FuncGetter:
-    def __init__(self, get):
-        self.get = get
-
-
-class BackendDummy:
-    """Helper to construct a minimal "backend" for testing.
-
-    Forwards any lookup to the class.  Documentation are used from
-    the function which must match in the name.
-    """
-    def __init__(self):
-        self.functions = FuncGetter(self.get_function)
-
-    @classmethod
-    def get_function(cls, name, default=None):
-        # Simply ignore the module for testing purposes.
-        _, name = name.split(":")
-
-        # Not get_identifier because it would find the super-class name.
-        res = {"function": f"{cls.__module__}:{cls.__name__}.{name}" }
-        if hasattr(cls, "uses_context"):
-            res["uses_context"] = cls.uses_context
-        if hasattr(cls, "should_run"):
-            res["should_run"] = get_identifier(cls.should_run)
-
-        func = getattr(cls, name)
-        if func.__doc__ is not None:
-            res["additional_docs"] = func.__doc__
-
-        return res
-
-    @classmethod
-    def dummy_func(cls, *args, **kwargs):
-        # Always define a small function that mainly forwards.
-        return cls.name, args, kwargs
+from spatch.testing import BackendDummy
 
 
 class IntB(BackendDummy):
