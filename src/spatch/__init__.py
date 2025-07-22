17f5b20918a6863b624e232656ebb271f22257af
Erik Welch <erik.n.welch@gmail.com>
2025-07-22 14:56:35 -0500

    Create and use `get_project_version` utility function

--- src/spatch/__init__.py
+++ src/spatch/__init__.py
@@ -1,18 +1,3 @@
-from importlib.metadata import version
-from .utils import from_identifier, get_identifier
+from .utils import from_identifier, get_identifier, get_project_version
 
-try:
-    __version__ = version("spatch")
-except ModuleNotFoundError:
-    import warnings
-
-    warnings.warn(
-        "No version metadata found for spatch, so `spatch.__version__` will be "
-        "set to None. This may mean that spatch was incorrectly installed or "
-        "not installed at all. For local development, consider doing an "
-        "editable install via `python -m pip install -e .` from within the "
-        "root `spatch/` repository folder."
-    )
-    __version__ = None
-    del warnings
-del version
+__version__ = get_project_version("spatch")
