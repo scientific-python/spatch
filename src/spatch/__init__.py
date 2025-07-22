8a97ad01d19659b0f9d26e9f51702c354c6bda47
Erik Welch <erik.n.welch@gmail.com>
2025-07-22 11:52:42 -0500

    Use hatch-vcs for dynamic version

--- src/spatch/__init__.py
+++ src/spatch/__init__.py
@@ -1,1 +1,18 @@
+from importlib.metadata import version
 from .utils import from_identifier, get_identifier
+
+try:
+    __version__ = version("spatch")
+except ModuleNotFoundError:
+    import warnings
+
+    warnings.warn(
+        "No version metadata found for spatch, so `spatch.__version__` will be "
+        "set to None. This may mean that spatch was incorrectly installed or "
+        "not installed at all. For local development, consider doing an "
+        "editable install via `python -m pip install -e .` from within the "
+        "root `spatch/` repository folder."
+    )
+    __version__ = None
+    del warnings
+del version
